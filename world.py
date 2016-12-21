89086ae87a0f7c369072d31f2bb89af5e54a9194
mameaw14 <pinyapornster@gmail.com>
2016-12-22 02:24:35 +0700

    reveal safe area

--- world.py
+++ world.py
@@ -6,6 +6,7 @@ class State(Enum):
     hidden = 1
     flagged = 2
     marked = 3
+    clear = 4
 
 class World(object):
     """world class"""
@@ -22,7 +23,7 @@ class Map(object):
         self.tiles_list = [[0] * col for i in range(row)]
         for i in range(0, row):
             for j in range(0, col):
-                tile = Tile(30 + j * 38 + (i % 2 == 0) * 19, 50 + i * 35)
+                tile = Tile(30 + j * 38 + (i % 2 == 0) * 19, 50 + i * 35, self.get_neighbor_list(i, j))
                 self.tiles_list[i][j] = tile
 
         self.calculate_status()
@@ -36,49 +37,16 @@ class Map(object):
         for row in self.tiles_list:
             for i in row:
                 if i.check_onclick(x, y):
-                    i.click()
                     self.update(self.tiles_list.index(row), row.index(i))
-
-    def update(self, i, j):
-        try:
-            self.tiles_list[i][j - 1].color = arcade.color.AFRICAN_VIOLET
-        except IndexError:
-            pass
-
-        try:
-            self.tiles_list[i][j + 1].color = arcade.color.AFRICAN_VIOLET
-        except IndexError:
-            pass
-
-        if i % 2 == 0:
-            j += 1
-
-        try:
-            self.tiles_list[i - 1][j - 1].color = arcade.color.AFRICAN_VIOLET
-        except IndexError:
-            pass
-
-        try:
-            self.tiles_list[i - 1][j].color = arcade.color.AFRICAN_VIOLET
-        except IndexError:
-            pass
-
-        try:
-            self.tiles_list[i + 1][j - 1].color = arcade.color.AFRICAN_VIOLET
-        except IndexError:
-            pass
-
-        try:
-            self.tiles_list[i + 1][j].color = arcade.color.AFRICAN_VIOLET
-        except IndexError:
-            pass
+                    i.click()
+                    return
 
     def get_neighbor_list(self, i, j):
         lis = [(i, j - 1), (i, j + 1)]
-        
+
         if i % 2 == 0:
             j += 1
-        
+
         lis.append((i - 1, j - 1))
         lis.append((i - 1, j))
         lis.append((i + 1, j - 1))
@@ -91,19 +59,35 @@ class Map(object):
 
         return lis
 
+    def update(self, i, j):
+        print("call function")
+        tile = self.tiles_list[i][j]
+        if tile.is_mine != True and tile.state != State.clear:
+            tile.color = arcade.color.AFRICAN_VIOLET
+            tile.state = State.clear
+            if tile.count_mine != 0:
+                return
+        else:
+            return
+
+        neighbor_list = self.tiles_list[i][j].neighbor_list
+        for x, y in neighbor_list:
+            self.update(x, y)
+
     def calculate_status(self):
         for i in range(0, self.row):
             for j in range(0, self.col):
                 if self.tiles_list[i][j].is_mine:
-                    neighbor_list = self.get_neighbor_list(i, j)
+                    neighbor_list = self.tiles_list[i][j].neighbor_list
                     for x, y in neighbor_list:
                         self.tiles_list[x][y].count_mine += 1
 
 class Tile(object):
     """hexagon blocks"""
-    def __init__(self, x, y):
+    def __init__(self, x, y, lis):
         self.x = x
         self.y = y
+        self.neighbor_list = lis
         self.is_mine = random.random() < 0.1
         self.state = State.hidden
         self.count_mine = 0
@@ -130,5 +114,5 @@ class Tile(object):
         return arcade.are_polygons_intersecting(self.point_list, ((x, y), (x - 1, y), (x, y + 1)))
 
     def click(self):
-        self.state = State.flagged
+        self.state = State.clear
 
